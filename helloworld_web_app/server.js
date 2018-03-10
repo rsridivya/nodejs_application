78c9bbd45a210d1f096637bf1ec76ea09e390d49
rsridivya <sridivya352@gmail.com>
2018-03-09 17:09:55 -0800

    Update server.js

--- helloworld_web_app/server.js
+++ helloworld_web_app/server.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
    // Send the response body as "Hello World"
-   response.end('Hi World');
+   response.end('Hello World');
 }).listen(8080);
 
 // Console will print the message
